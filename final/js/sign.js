c7f80bbfdcce116a030e55e0d93b9333b017554e
joseph1024 <61818818+joseph1024@users.noreply.github.com>
2020-05-29 11:28:02 +0800

    final

--- final/js/sign.js
+++ final/js/sign.js
@@ -1,10 +1,22 @@
+	
+	/*sign in form*/
+	var modal = document.getElementById('sign');
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
 
 
-var modal = document.getElementById('sign');
-
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+	/*paswword invisable*/
+    var demoImg = document.getElementById("demo_img");
+    var demoInput = document.getElementById("demo_input");
+    function hideShowPsw(){
+        if (demoInput.type == "password") {
+            demoInput.type = "text";
+            demoImg.className = "fas fa-eye-slash"
+        }else {
+            demoInput.type = "password";
+            demoImg.className = "fa fa-eye ml-2"
+        }
+    }
